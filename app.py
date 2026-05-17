ed17342f75dd8e30b35af127c3736d263384c60a
Maulana Ihsan Ahmad <maulanaihsanpress@gmail.com>
2026-05-17 19:15:01 +0700

    Enable Streamlit auth login

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import secrets
 import shutil
 
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload, MediaIoBaseDownload
